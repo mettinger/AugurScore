7e208b94cfcdd0a6675561179788cc30f792805c
mettinger <jettinger35@gmail.com>
2019-01-03 11:16:00 -0500

    message

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='AugurScore')
